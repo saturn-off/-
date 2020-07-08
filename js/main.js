c8d4726684c1d29dde5ddc78871978c600ea9b28
saturn-off <68026991+saturn-off@users.noreply.github.com>
2020-07-08 20:59:02 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log('It works!')
+$('.github').click(function(){
+	window.location.href = "https://github.com/Lokilife/"
+})
+$('.discord').click(function(){
+	window.location.href = "https://discord.gg/k5k2pbE"
+})
